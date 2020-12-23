66b7fe1e3ea41190ad604f2afe215dff45d2ed08
Danny Garber <dannyg@outlook.com>
2020-12-23 01:36:06 -0500

    reformated Dashboard, added game selection

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useRef, useState } from 'react'
-import { Text } from '@fluentui/react'
+import { Dropdown, Text, initializeIcons, IDropdown, IStackTokens, Stack, IStackItemStyles, IDropdownOption } from '@fluentui/react'
 import '../Styles/App.css';
 import 'office-ui-fabric-react/dist/css/fabric.css';
 import { authProvider } from '../Auth/AuthProvider'
@@ -16,18 +16,39 @@ import { navBarContext } from '../NavBar/NavBar.Context'
 import { useMenu } from '../NavBar/NavBar.Hook'
 import { NIL } from 'uuid';
 
+const dropdownStyles = { dropdown: { width: 500 }, label: { color: 'White' } };
+const nonShrinkingStackItemStyles: IStackItemStyles = {
+  root: {
+    alignItems: 'center',
+    display: 'flex',
+    height: 500,
+    justifyContent: 'center',
+    overflow: 'hidden',
+    width: 5000,
+  },
+}
+
+// Initialize icons in case this page uses them
+initializeIcons();
+
 const Dashboard: React.FC = () => {
+  const [selectedGame, setSelectedGame] = useState<IDropdownOption>()
   const [events, setEvents] = useState<IEvent[]>([])
   const [newEvent, setNewEvent] = useState<IEvent | null>(null)
-  const [eventCount, addEventCount] = useState(0);
-  const [toggled, setToggled] = useState(false);
+  const [eventCount, addEventCount] = useState(0)
+  const [toggled, setToggled] = useState(false)
 
-  
   const [token, setToken] = useState<string>('')
+
 
   const userName = authProvider.getAccountInfo()?.account.userName
   const name = authProvider.getAccountInfo()?.account.name
   const accountId = authProvider.getAccountInfo()?.account.accountIdentifier
+
+  const dropdownRef = React.createRef<IDropdown>();
+  const onSetFocus = () => dropdownRef.current!.focus(true);
+
+  const stackTokens: IStackTokens = { childrenGap: 20 };
 
   authProvider.getAccessToken().then ((value: AccessTokenResponse) => {
     setToken(value.accessToken)
@@ -37,6 +58,7 @@ const Dashboard: React.FC = () => {
     fetchEvents().then((totalEvents: number) => {
       addEventCount(totalEvents)  
     })
+    onSetFocus()
   },[eventCount])
 
 
@@ -64,24 +86,10 @@ const Dashboard: React.FC = () => {
   const handleAddEvent = (e: React.FormEvent, formData: IEvent): void => {
     e.preventDefault()
     const _form: any = e.currentTarget
-    formData = {...formData, eventType: _form.elements[2].value}
-    console.log("adding new event")
+    formData = {...formData, eventType: _form.elements[2].value, position: formData.position ?? 0, significance: formData.significance ?? 0}
     setNewEvent(formData)
     setToggled(true)
   }
-
-  // const handleAddEvent = async (e: React.FormEvent, formData: IEvent): Promise<void> => {
-  //   e.preventDefault()
-  //   const _form: any = e.currentTarget
-  //   formData = {...formData, eventType: _form.elements[2].value}
-  //   await addEvent(formData, events)
-  //   .then(({ data }) => {
-  //     setEvents(data.events)
-  //     console.log(`added total of ${events.length} events`)
-  //     addEventCount(data.events.length)
-  //   })
-  //   .catch((err) => console.log(err))
-  // }
 
   const handleSaveEvent = async (e: React.FormEvent, formData: IEvent): Promise<void> => {
     e.preventDefault()
@@ -119,29 +127,54 @@ const Dashboard: React.FC = () => {
     .catch((err) => console.log(err))
   }
 
+  const onGameChanged = (event: React.FormEvent<HTMLDivElement>, item?: IDropdownOption, index?: number): void => {
+    setSelectedGame(item)
+    console.log(`Selected: ${item?.text}`)
+  }
+
   return (
     <div className="ms-Grid" dir="ltr">
         <div className="ms-Grid-row">
           <div className="ms-Grid-col ms-sm1 ms-xl1">
             <Navigation selectedKey="dash" />
           </div>
-          <div className="main-element ms-Grid-col ms-sm11 ms-xl11">
-            <Text block className="Title" variant='xxLarge'>Chelsea vs Barcelona, February, 21, 2021</Text>
-            <div className="ms-Grid-row">
+          <Stack tokens={stackTokens} verticalAlign="end">
+            <Stack.Item align="auto">
+              <Stack horizontalAlign="center">
+                <Dropdown
+                  componentRef={dropdownRef}
+                  placeholder="Select a game"
+                  label="Select a game for which you want to edit significant events"
+                  options={[
+                    { key: 'CHEBAR', text: 'Chelsea vs Barcelona' },
+                    { key: 'ATLSEV', text: 'Athletico vs Sevilia' },
+                    { key: 'LIVMC', text: 'Liverpool vs Manchester City' },
+                    { key: 'MANARC', text: 'Manchester United vs Arsenal' },
+                  ]}
+                  required
+                  styles={dropdownStyles}
+                  onChange={onGameChanged}
+                />
+              </Stack>
+            </Stack.Item>
+            <Stack.Item align="auto">
+              <Text block className="Title" variant='xxLarge'>{selectedGame?.text}</Text>
+            </Stack.Item>
+            <Stack.Item align="auto">
               <main className='App'>
                 <AddEvent saveEvent={handleAddEvent} />
               </main>
-            </div>
-            <div className="ms-Grid-row">
+            </Stack.Item>
+            <Stack.Item align="stretch">
               {!toggled && <main className='App'>
-                  <EventTable 
-                    events={events} 
-                    updateEvent={handleUpdateEvent}
-                    deleteEvent={handleDeleteEvent}
-                  />                  
+                <EventTable 
+                  events={events} 
+                  updateEvent={handleUpdateEvent}
+                  deleteEvent={handleDeleteEvent}
+                />                  
               </main>}
-            </div>
-          </div>
+            </Stack.Item>
+          </Stack>
         </div>
         <footer>
           <h2>Environment: {process.env.NODE_ENV}</h2>
